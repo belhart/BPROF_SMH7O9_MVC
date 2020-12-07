8e7d3acba67464775c821126faea2405fa4fab48
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-12-07 15:20:41 +0100

    team web model added

---
+++ CSHARP/OENIK_PROG3_2019_2_SMH7O9/F1Stats.Web/Models/Csapat.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace F1Stats.Web.Models
+{
+    public class Csapat
+    {
+        [Display(Name = "Csapat neve")]
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public int CsapatNev { get; set; }
+
+        [Display(Name = "Csapat motor szallitoja")]
+        [Required]
+        public int Motor { get; set; }
+
+        [Display(Name = "Csapat versenyeinek szama")]
+        [Required]
+        public int VersenyekSzama { get; set; }
+
+        [Display(Name = "Csapat osszes gyozelme")]
+        [Required]
+        public int Gyozelmek { get; set; }
+    }
+}
