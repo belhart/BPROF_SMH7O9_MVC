2501b01ad7a2debd2851a7b6b32e330cf4d91efe
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-12-07 15:23:16 +0100

    teamviewmodel added

---
+++ CSHARP/OENIK_PROG3_2019_2_SMH7O9/F1Stats.Web/Models/CsapatViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace F1Stats.Web.Models
+{
+    public class CsapatViewModel
+    {
+        public Csapat EditedCsapat { get; set; }
+        public List<Csapat> ListOfCsapatok { get; set; }
+    }
+}
