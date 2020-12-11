61479ceed3c69e1a68d25f6edbddd179db22ea61
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-12-11 13:17:34 +0100

    dbconext and components added

---
+++ CSHARP/BPROF_SHM7O9_MVC/F1Stats.Data/F1StatsDbContext.cs
@@ -0,0 +1,31 @@
+﻿using F1Stats.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1Stats.Data
+{
+    class F1StatsDbContext : DbContext
+    {
+        public F1StatsDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.
+                    UseLazyLoadingProxies().
+                    UseSqlServer(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\F1StatsDatabase.mdf;integrated security=True;MultipleActiveResultSets=True");
+            }
+        }
+
+        public virtual DbSet<Csapat> Csapat { get; set; }
+        public virtual DbSet<Eredmeny> Eredmeny { get; set; }
+        public virtual DbSet<Versenyzo> Versenyzo { get; set; }
+        public virtual DbSet<Versenyhetvege> Versenyhetvege { get; set; }
+    }
+}
