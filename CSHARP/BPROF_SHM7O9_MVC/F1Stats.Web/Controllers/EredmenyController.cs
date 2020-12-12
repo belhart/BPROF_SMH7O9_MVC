89bb143a6f8ef9ec666b5168e67cd109eabb2a33
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-12-12 17:39:27 +0100

    result page added with error throwing fucntions

---
+++ CSHARP/BPROF_SHM7O9_MVC/F1Stats.Web/Controllers/EredmenyController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using F1Stats.Data.Models;
+using F1Stats.Logic;
+using F1Stats.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
+
+namespace F1Stats.Web.Controllers
+{
+    public class EredmenyController : Controller
+    {
+        IEredmenyLogic logic;
+        EredmenyViewModel vm;
+
+        public EredmenyController(EredmenyLogic eredmenyLogic)
+        {
+            this.logic = eredmenyLogic;
+
+            vm = new EredmenyViewModel();
+            vm.EditedEredmeny = new Eredmeny();
+            var eredmenyek = logic.GetAllEredmeny();
+            vm.ListOfEredmenyek = eredmenyek.ToList();
+        }
+        private Eredmeny GetEredmenyModel(int id)
+        {
+            return this.logic.GetOneEredmeny(id);
+        }
+
+        // GET: Eredmeny
+        public ActionResult Index()
+        {
+            ViewData["editAction"] = "AddNew";
+            return View("EredmenyIndex", vm);
+        }
+
+        // GET: Eredmeny/Details/5
+        public ActionResult Details(int id)
+        {
+            return View("EredmenyDetails", GetEredmenyModel(id));
+        }
+        //GET
+        public ActionResult Remove(int id)
+        {
+            TempData["editResult"] = "Delete FAIL";
+            if (logic.DeleteEredmeny(id)) TempData["editResult"] = "Delete OK";
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ViewData["editAction"] = "Edit";
+            vm.EditedEredmeny = GetEredmenyModel(id);
+            return View("EredmenyIndex", vm);
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Edit(Eredmeny eredmeny, string editAction)
+        {
+            if (ModelState.IsValid && eredmeny != null)
+            {
+                TempData["editResult"] = "Edit OK";
+                if (editAction == "AddNew")
+                {
+                    logic.CreateEredmeny(eredmeny);
+                }
+                else
+                {
+                    bool success = logic.UpdateEredmeny(csapat.csapat_nev, csapat.motor, csapat.versenyek_szama, csapat.gyozelmek);
+                    if (!success) TempData["editResult"] = "Edit FAIL";
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ViewData["editAction"] = "Edit";
+                vm.EditedEredmeny = eredmeny;
+                return View("EredmenyIndex", vm);
+            }
+        }
+    }
+}
---
+++ CSHARP/BPROF_SHM7O9_MVC/F1Stats.Web/Models/EredmenyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using F1Stats.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace F1Stats.Web.Models
+{
+    public class EredmenyViewModel
+    {
+        public Eredmeny EditedEredmeny { get; set; }
+        public List<Eredmeny> ListOfEredmenyek { get; set; }
+    }
+}
