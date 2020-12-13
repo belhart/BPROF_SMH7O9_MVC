4eab1bad08324b04448b90dc91bce7f508c3f295
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-12-13 18:02:23 +0100

    not crud function implemented on homepage

--- CSHARP/BPROF_SHM7O9_MVC/F1Stats.Web/Controllers/HomeController.cs
+++ CSHARP/BPROF_SHM7O9_MVC/F1Stats.Web/Controllers/HomeController.cs
@@ -6,21 +6,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using F1Stats.Web.Models;
+using F1Stats.Logic;
 
 namespace F1Stats.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private HomeViewModel vm;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            this.vm = new HomeViewModel();
+            vm.TeamWithMostPoints = OsszetettLogic.GetTeamWithMostPoints();
+            vm.ListOfSumPointResult = OsszetettLogic.GetDriversPoints().ToList();
+            vm.ListOfEnginePointResult = OsszetettLogic.GetResultWithEngineNames(15).ToList();
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index", vm);
         }
 
         public IActionResult Privacy()
