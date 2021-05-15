7b83f6dd1ed1a816e0aee9637441bc25142c6e96
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-05-15 16:29:44 +0200

    models added for auth logic

---
+++ BPROF_SHM7O9_MVC/F1Stats.Data/Models/TokenModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1Stats.Data
+{
+    public class TokenModel
+    {
+        /// <summary>
+        /// JWT token granted to the user
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Expiration date of the token
+        /// </summary>
+        public DateTime ExpirationDate { get; set; }
+    }
+}
