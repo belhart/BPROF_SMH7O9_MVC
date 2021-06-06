9790408fff787147bb9debdb1232ce3dce6a02a8
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-06-06 13:32:30 +0200

    jsonignore attribue added for virtual props

--- BPROF_SHM7O9_MVC/F1Stats.Data/Models/Versenyzo.cs
+++ BPROF_SHM7O9_MVC/F1Stats.Data/Models/Versenyzo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace F1Stats.Data.Models
 {
@@ -20,7 +21,9 @@ namespace F1Stats.Data.Models
         public int ossz_pont { get; set; }
         [NotNull]
         public int idenybeli_pont { get; set; }
+        [JsonIgnore]
         public virtual Csapat Csapat { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Eredmeny> Eredmeny { get; set; }
     }
 }
