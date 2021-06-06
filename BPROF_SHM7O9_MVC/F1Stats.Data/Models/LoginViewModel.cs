2127763d431a2295a8c2829fb5b1a1f7ad891f09
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-06-06 18:55:25 +0200

    login view model added

---
+++ BPROF_SHM7O9_MVC/F1Stats.Data/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1Stats.Data.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
