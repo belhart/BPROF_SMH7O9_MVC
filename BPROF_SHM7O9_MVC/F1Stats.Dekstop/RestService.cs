f8eea54646c5a4544980924b225ca185b15970f3
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-06-06 18:52:36 +0200

    service to handle http requests added

---
+++ BPROF_SHM7O9_MVC/F1Stats.Dekstop/RestService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace F1Stats.Dekstop
+{
+    public class RestService
+    {
+        HttpClient client;
+        string endpoint = "https://api.android.2u.si/";
+        public RestService(string baseurl, string endpoint, string token = "")
+        {
+            this.client = new HttpClient();
+            this.client.BaseAddress = new Uri(baseurl);
+            this.client.DefaultRequestHeaders.Accept.Clear();
+            this.client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue
+                ("application/json"));
+
+            if (token != "")
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+    }
+}
