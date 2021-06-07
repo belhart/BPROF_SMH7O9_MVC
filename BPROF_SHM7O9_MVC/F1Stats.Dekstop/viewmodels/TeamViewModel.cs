286df347bf6d3f81f5c8332711eebc45138b2af4
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-06-07 19:22:34 +0200

    teamviewmodel edited to save token

--- BPROF_SHM7O9_MVC/F1Stats.Dekstop/viewmodels/TeamViewModel.cs
+++ BPROF_SHM7O9_MVC/F1Stats.Dekstop/viewmodels/TeamViewModel.cs
@@ -8,5 +8,15 @@ namespace F1Stats.Dekstop.viewmodels
 {
     public class TeamViewModel
     {
+        private string _token;
+        public string TOKEN
+        {
+            get { return _token; }
+            set { _token = value; }
+        }
+        public TeamViewModel(string token)
+        {
+            this.TOKEN = token;
+        }
     }
 }
