00b288adbd7faf5678449a66452274c2caf0d57e
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-06-08 16:38:10 +0200

    raceweekend edit and delete implemented

--- BPROF_SHM7O9_MVC/F1Stats.Dekstop/views/RaceWeekendView.xaml.cs
+++ BPROF_SHM7O9_MVC/F1Stats.Dekstop/views/RaceWeekendView.xaml.cs
@@ -23,6 +23,7 @@ namespace F1Stats.Dekstop.views
     public partial class RaceWeekendView : UserControl
     {
         private string token;
+        IEnumerable<Versenyhetvege> cacheRaceWeekendteamList;
         public RaceWeekendView()
         {
             InitializeComponent();
@@ -32,6 +33,7 @@ namespace F1Stats.Dekstop.views
             DGrid1.ItemsSource = null;
             RestService restService = new RestService("/Versenyhetvege", token);
             IEnumerable<Versenyhetvege> teamList = await restService.Get<Versenyhetvege>();
+            this.cacheRaceWeekendteamList = teamList;
             DGrid1.ItemsSource = teamList;
         }
 
@@ -39,6 +41,73 @@ namespace F1Stats.Dekstop.views
         {
             this.token = ((RaceWeekendViewModel)this.DataContext).TOKEN;
             this.RefreshTeamList();
+        }
+
+        private void DGrid1_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Do you want to save the changes made?", "Save changes", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                var editedValue = (e.EditingElement as TextBox).Text;
+                int raceWeekendNumber = (e.Row.DataContext as Versenyhetvege).VERSENYHETVEGE_SZAMA;
+                Versenyhetvege newRaceWeekend = (e.Row.DataContext as Versenyhetvege);
+                switch (e.Column.SortMemberPath)
+                {
+                    case "nev": newRaceWeekend.nev = editedValue; break;
+                    case "VERSENYHETVEGE_SZAMA": newRaceWeekend.VERSENYHETVEGE_SZAMA = int.Parse(editedValue); break;
+                    case "hossz": newRaceWeekend.hossz = int.Parse(editedValue); break;
+                    case "kor": newRaceWeekend.kor = int.Parse(editedValue); break;
+                    case "idopont": newRaceWeekend.idopont = DateTime.Parse(editedValue); break;
+                    case "helyszin": newRaceWeekend.helyszin = editedValue; break;
+                    default: MessageBox.Show("Something went wrong"); return;
+                }
+                this.UpdateTeamFromList(raceWeekendNumber, newRaceWeekend);
+                return;
+            }
+            DGrid1.ItemsSource = null;
+            DGrid1.ItemsSource = cacheRaceWeekendteamList;
+        }
+
+        private void DGrid1_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Right) return;
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Are you sure you want to delete the item?", "Delete item", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult != MessageBoxResult.Yes) return;
+            DataGrid grid = sender as DataGrid;
+            if (grid != null && grid.SelectedItems != null && grid.SelectedItems.Count == 1)
+            {
+                DataGridRow dgr = grid.ItemContainerGenerator.ContainerFromItem(grid.SelectedItem) as DataGridRow;
+                Versenyhetvege versenyhetvege = (Versenyhetvege)dgr.Item;
+                this.DeleteTeamFromList(versenyhetvege.VERSENYHETVEGE_SZAMA);
+            }
+        }
+
+        private void DeleteTeamFromList(int raceWeekendNumber)
+        {
+            RestService restService = new RestService("/Versenyhetvege", token);
+            try
+            {
+                restService.Delete<int>(raceWeekendNumber);
+                MessageBox.Show("Race weekend successfully deleted");
+                this.RefreshTeamList();
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong or you dont have access to this action.");
+            }
+        }
+
+        private void UpdateTeamFromList(int raceWeekendNumber, Versenyhetvege newRaceWeekend)
+        {
+            RestService restService = new RestService("/Versenyhetvege", token);
+            try
+            {
+                restService.Put<int, Versenyhetvege>(raceWeekendNumber, newRaceWeekend);
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong or you dont have access to this action.");
+            }
         }
     }
 }
