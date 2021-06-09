e3f4a8a1af79b5ae1a9fe0adda715b48d27b7487
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-06-09 09:47:18 +0200

    base of creating a new team added

--- BPROF_SHM7O9_MVC/F1Stats.Dekstop/views/TeamView.xaml.cs
+++ BPROF_SHM7O9_MVC/F1Stats.Dekstop/views/TeamView.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -108,6 +109,25 @@ namespace F1Stats.Dekstop.views
             {
                 MessageBox.Show("Something went wrong or you dont have access to this action.");
             }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Reset_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.tnTextBox.Text = "";
+            this.wTextBox.Text = "";
+            this.NoRTextBox.Text = "";
+            this.EnTextBox.Text = "";
+        }
+
+        private void Create_Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
